6a98b98cf731e77cfb4a9406246890c0739db545
SashaMelva <sa1524517@gmail.com>
2023-07-21 14:16:48 +0300

    employee base data

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\EmployeesResource;
+use App\Http\Services\BaseApi;
+use App\Models\Employee;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $employees = Employee::all();
+        return (new BaseApi())->sendResponse(EmployeesResource::collection($employees))->getData();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(EmployeeRequest $request)
+    {
+        $validated = $request->validated();
+        Employee::created($validated);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Employee $employee)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Employee $employee)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Employee $employee)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Employee $employee)
+    {
+        $employee = Employee::find($employee);
+        $employee->delete();
+    }
+}
