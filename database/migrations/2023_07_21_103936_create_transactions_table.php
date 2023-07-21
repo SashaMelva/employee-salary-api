069434e6b59e23972baa43cf5694f53c9c055a33
SashaMelva <sa1524517@gmail.com>
2023-07-21 13:57:43 +0300

    migration refactoring

--- database/migrations/2023_07_21_103936_create_transactions_table.php
+++ database/migrations/2023_07_21_103936_create_transactions_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->time('hours');
             $table->float('price');
             $table->timestamps();
-            $table->timestamps();
         });
     }
 
