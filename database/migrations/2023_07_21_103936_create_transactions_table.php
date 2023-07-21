a43a89244860e5bb69620db4f0fbcc0456e8995a
SashaMelva <sa1524517@gmail.com>
2023-07-21 13:43:50 +0300

    creating table db

---
+++ database/migrations/2023_07_21_103936_create_transactions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('employee_id');
+            $table->integer('status_id');
+            $table->time('hours');
+            $table->float('price');
+            $table->timestamps();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +13,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('status_transactions')->insert([
+            'title' => 'Выполнено',
+            'description' => '',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('status_transactions')->insert([
+            'title' => 'Выполняется',
+            'description' => '',
+        ]);
+
+        DB::table('status_transactions')->insert([
+            'title' => 'Ожидание',
+            'description' => '',
+        ]);
     }
 }
